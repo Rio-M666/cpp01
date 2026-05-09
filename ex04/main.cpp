bd1bb2dcd0ed1989964a2a850703223c4dc1f717
Miyachi Rio <mrio@c6r2s8.42tokyo.jp>
2026-05-09 21:27:44 +0900

    save

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,6 +1,17 @@
+#include <iostream>
+
 int	main(int ac, char *av[])
 {
+	std::string filename;
+	std::string s1;
+	std::string s2;
 	if (ac != 4)
 		return (1);
-    
+	filename = av[1];
+	s1 = av[2];
+	s2 = av[3];
+
+	if (s1.empty())
+		return (1);
+	
 }
