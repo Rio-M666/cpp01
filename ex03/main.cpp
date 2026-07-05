d538766a2097986ffef356d1f48f2283aefde2de
Miyachi Rio <mrio@c1r1s6.42tokyo.jp>
2026-07-05 14:48:56 +0900

    sve

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -3,12 +3,23 @@
 
 int	main(void)
 {
-	Weapon club("crude spiked club");
+	{
+		Weapon club = Weapon("crude spiked club");
 
-	HumanB jim("Jim");
-	jim.setWeapon(club);
-	jim.attack();
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
 
-	club.setType("some other type of club");
-	jim.attack();
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+
+	return (0);
 }
